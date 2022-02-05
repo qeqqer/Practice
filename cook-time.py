5aecc63330d5968fd44014b3147f0e61d59352c5
dwhailey <shindw0314@gmail.com>
2022-02-05 01:18:23 -0700

    practice 1

---
+++ cook-time.py
@@ -0,0 +1,26 @@
+h, m = map(int,input().split()) #A=hour B=min
+cook_min = int(input()) #need min
+
+time = m+cook_min
+
+if (time<=59):
+	print(h,time)
+elif(time>59):
+	
+	plus_h = int(time/60)
+	final_h = h+plus_h
+
+	final_h=plus_h+h
+	if(final_h>=24):
+		final_h -= 24
+	
+	
+	plus_m = time%60
+	final_m = plus_m
+	if(final_m==60):
+		final_h+=1
+		final_m==0
+
+
+	print(final_h,final_m)
+
